77a565a93b2af0bfb958d84a679c5178775c46fb
ridha-muhammad <ridha.mohd01@gmail.com>
2024-07-12 21:34:45 +0530

    initial upload after completion

--- server.js
+++ server.js
@@ -3,28 +3,40 @@ const cors = require("cors");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
 const colors = require("colors");
+const path = require("path");
 const connectDb = require("./config/connectDb");
+
 // config dot env file
 dotenv.config();
 
-//databse call
+// database call
 connectDb();
 
-//rest object
+// rest object
 const app = express();
 
-//middlewares
+// middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-//routes
+// routes
+// user routes
 app.use("/api/v1/users", require("./routes/userRoute"));
+// transections routes
+app.use("/api/v1/transections", require("./routes/transectionRoutes"));
 
-//port
+// static files
+app.use(express.static(path.join(__dirname, "./client/build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// port
 const PORT = 8080 || process.env.PORT;
 
-//listen server
+// listen server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
